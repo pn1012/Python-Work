9a37f387ccaa40da6285e5c7a86e645a7f033a30
pn1012 <pn1012@gmail.com>
2015-03-03 17:29:56 -0600

    updated

--- Pop_Growth.py
+++ Pop_Growth.py
@@ -7,11 +7,11 @@ with open('/Users/patrickcorynichols/Projects/Thinkful/lecz-urban-rural-populati
     for line in inputFile:
         line = line.rstrip().split(',')
         line[5] = int(line[5])
-        line[6] = int(line[6])
+        line[7] = int(line[7])
         if line[1] == 'Total National Population':
-            population_dict[line[0]] += float(line[6] - line[5])
+            population_dict[line[0]] += float(line[5])/float(line[7])
 
-with open('/Users/patrickcorynichols/Projects/Thinkful/national_population.csv', 'w') as outputFile:
+with open('/Users/patrickcorynichols/Projects/Thinkful/national_population_density.csv', 'w') as outputFile:
     outputFile.write('continent,Pop_Growth\n')
     for k, v in population_dict.iteritems():
         outputFile.write(k + ',' + str(v) + '\n')
